6bc935e814e11469e1ae0b3a25e30a2a00a13a7f
Graeme Jury <gvjury@gmail.com>
2011-11-09 14:17:35 +1300

    allowTx defaults to false and setting is sticky

--- trunk/src/QtRadio/Configure.cpp
+++ trunk/src/QtRadio/Configure.cpp
@@ -238,6 +238,10 @@ void Configure::loadSettings(QSettings* settings) {
     settings->beginGroup("SDROM");
     if(settings->contains("threshold")) widget.nbThresholdSpinBox->setValue(settings->value("threshold").toInt());
     settings->endGroup();
+
+    settings->beginGroup("TxSettings");
+    widget.allowTx->setChecked(settings->value("allowTx",FALSE).toBool());
+    settings->endGroup();
 }
 
 void Configure::saveSettings(QSettings* settings) {
@@ -287,6 +291,9 @@ void Configure::saveSettings(QSettings* settings) {
     settings->endGroup();
     settings->beginGroup("SDROM");
     settings->setValue("threshold",widget.nbThresholdSpinBox->value());
+    settings->endGroup();
+    settings->beginGroup("TxSettings");
+    settings->setValue("allowTx",widget.allowTx->checkState());
     settings->endGroup();
 }
 
