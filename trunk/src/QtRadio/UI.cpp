2aed248846ccf515333c54573eae8e5e56eca758
Alex Lee <alexlee188@gmail.com>
2011-12-23 18:55:48 +0800

    QtRadio: date 23 Dec 2011

--- trunk/src/QtRadio/UI.cpp
+++ trunk/src/QtRadio/UI.cpp
@@ -1943,7 +1943,7 @@ void UI::getMeterValue(int m, int s)
 
 void UI::printWindowTitle(QString message)
 {
-    setWindowTitle("QtRadio - Server: " + configure.getHost() + "(Rx "+ QString::number(configure.getReceiver()) +") .. " + message + " rxtx-event RTP 21 Dec 2011");
+    setWindowTitle("QtRadio - Server: " + configure.getHost() + "(Rx "+ QString::number(configure.getReceiver()) +") .. " + message + "  - rxtx-event RTP 23 Dec 2011");
 }
 
 void UI::printStatusBar(QString message)
