daf2c1a490d8dc062e7e8635861016bf7cc1dae6
Alex Lee <alexlee188@gmail.com>
2011-12-26 23:52:32 +0800

    softrock: resolve merge conflict of jack code.  Compiled but not tested yet.

--- trunk/src/softrock/client.c
+++ trunk/src/softrock/client.c
@@ -193,19 +193,17 @@ void* audio_thread(void* arg) {
     int old_state, old_type;
     int bytes_read;
     int on=1;
-<<<<<<< HEAD
+
 	int error_no;
 	int  pipe_left = *softrock_get_jack_write_pipe_left(rx->client->receiver);
 	int  pipe_right = *softrock_get_jack_write_pipe_right(rx->client->receiver);
 	int num_bytes = sizeof(float)*BUFFER_SIZE;
 	int blocked_num = 0;
 static int second_time = 0;
-=======
     BUFFER buffer;
     unsigned long sequence=0L;
     unsigned short offset=0;;
 
->>>>>>> bdf88e7484fe1ec82cbf943217c4bf2ab2dbfa5e
 
     if (softrock_get_verbose()) fprintf(stderr,"audio_thread port=%d\n",audio_port+(rx->id*2));
 	
@@ -271,7 +269,7 @@ static int second_time = 0;
             perror("recvfrom socket failed for audio buffer");
             exit(1);
         }
-<<<<<<< HEAD
+#endif
 		if (softrock_get_jack () == 1) { 
 			if (second_time == 32) {// This is for testing only.  It will fail the second connection from QtRadio.
 				softrock_set_client_active_rx(rx->client->receiver, ADD_RX);	
@@ -299,19 +297,8 @@ static int second_time = 0;
 		} else {
         	process_softrock_output_buffer(rx->output_buffer,&rx->output_buffer[BUFFER_SIZE]);
 		}
-=======
-#endif
 
-/*
-#ifdef JACKAUDIO
-	if(softrock_get_jack () == 1) 
-#endif
-*/
-	{
-        process_softrock_output_buffer(rx->output_buffer,&rx->output_buffer[BUFFER_SIZE]);
-	}
->>>>>>> bdf88e7484fe1ec82cbf943217c4bf2ab2dbfa5e
-    }
+    	}
 	softrock_set_client_active_rx (rx->client->receiver, DEC_RX); 
 }
 
