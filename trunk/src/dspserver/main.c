ae076fd2b6005b08cec8a39e090736103429059b
Alex Lee <alexlee188@gmail.com>
2012-01-25 15:28:01 +0800

    dspserver: testing SetCorrectRXIQMu to 0.25

--- trunk/src/dspserver/main.c
+++ trunk/src/dspserver/main.c
@@ -262,6 +262,9 @@ int main(int argc,char* argv[]) {
     SetTXOsc(1, LO_offset);
     SetTXAMCarrierLevel(1, 0.5);
 
+    SetCorrectRXIQMu (0, 0, 0.25);
+    SetCorrectIQEnable(1);
+
     tx_init();	// starts the tx_thread
 
     while(1) {
