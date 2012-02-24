ac0806c102dc43343c35602a298c4af690da2200
Ethan Blanton <elb@psg.com>
2012-02-23 21:48:09 -0500

    Reduce globals used in main.c and option processing

--- trunk/src/dspserver/register.h
+++ trunk/src/dspserver/register.h
@@ -4,16 +4,14 @@
 #if ! defined __REGISTER_H__
 #define __REGISTER_H__
 
-void init_register();
+void init_register(const char *scfile);
 void updateStatus(char *status);
 void close_register();
 void doRemove();
 int chkPasswd(char *user, char *pass);
 int chkFreq(char *user,  long long freq2chk, int mode);
 
-char share_config_file[256];
-char *home;
-int toShareOrNotToShare;
+extern int toShareOrNotToShare;
 
 #define TXALL  0
 #define TXPASSWD  1
