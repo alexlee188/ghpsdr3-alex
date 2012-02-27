2eee231e89b42310c906b223a051ab76cc912dd8
Alex Lee <alexle188@gmail.com>
2012-02-27 23:31:40 +0800

    QtRadio: rotating cube with waterfall texture

--- trunk/src/QtRadio/Waterfallcl.cpp
+++ trunk/src/QtRadio/Waterfallcl.cpp
@@ -285,36 +285,36 @@ void Waterfallcl::paintGL()
     glTexCoord2f(1.0f, h); glVertex3f( 1.0f,  1.0f,  1.0f);
     glTexCoord2f(0.0f, h); glVertex3f(-1.0f,  1.0f,  1.0f);
     // Back Face
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glTexCoord2f(0.0f, h); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(0.0f, h + 0.5f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glTexCoord2f(1.0f, h + 0.5f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    glTexCoord2f(1.0f, h); glVertex3f( 1.0f, -1.0f, -1.0f);
     // Top Face
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    glTexCoord2f(0.0f, h); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glTexCoord2f(0.0f, h + 0.5f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glTexCoord2f(1.0f, h + 0.5f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    glTexCoord2f(1.0f, h); glVertex3f( 1.0f,  1.0f, -1.0f);
     // Bottom Face
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    glTexCoord2f(0.0f, h + 0.5f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, h + 0.5f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, h); glVertex3f( 1.0f, -1.0f,  1.0f);
+    glTexCoord2f(0.0f, h); glVertex3f(-1.0f, -1.0f,  1.0f);
     // Right face
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    glTexCoord2f(1.0f, h + 0.5f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glTexCoord2f(1.0f, h); glVertex3f( 1.0f,  1.0f, -1.0f);
+    glTexCoord2f(0.0f, h); glVertex3f( 1.0f,  1.0f,  1.0f);
+    glTexCoord2f(0.0f, h + 0.5f); glVertex3f( 1.0f, -1.0f,  1.0f);
     // Left Face
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glTexCoord2f(1.0f, h); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glTexCoord2f(0.0f, h); glVertex3f(-1.0f, -1.0f,  1.0f);
+    glTexCoord2f(0.0f, h + 0.5f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glTexCoord2f(1.0f, h + 0.5f); glVertex3f(-1.0f,  1.0f, -1.0f);
     glEnd();
 
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_TEXTURE_2D);
 
-    //rquad -= 0.5f;
+    rquad -= 0.2f;
 
 }
 
