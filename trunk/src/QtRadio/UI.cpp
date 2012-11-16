a59ebfd216b3975053c4dc02623210b855df895f
Alex Lee <alexlee188@gmail.com>
2012-11-16 23:23:41 +0800

    updated date to 2012 Nov 16

--- trunk/src/QtRadio/UI.cpp
+++ trunk/src/QtRadio/UI.cpp
@@ -2080,7 +2080,7 @@ void UI::printWindowTitle(QString message)
     }
     setWindowTitle("QtRadio - Server: " + servername + " " + configure.getHost() + "(Rx "
                    + QString::number(configure.getReceiver()) +") .. "
-                   + getversionstring() +  message + "  master 4 Nov 2012");
+                   + getversionstring() +  message + "  master 16 Nov 2012");
     lastmessage = message;
 
 }
--- trunk/src/dspserver/main.c
+++ trunk/src/dspserver/main.c
@@ -79,7 +79,7 @@
 * 
 */
 
-const char *version = "20121111;-dspserver-resampling"; //YYYYMMDD; text desc
+const char *version = "20121116;-master"; //YYYYMMDD; text desc
 
 // main.c
 
