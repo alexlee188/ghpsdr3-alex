6a3d4c1b7270126068d0f31db23f5a4fd1d97563
Alex Lee <alexlee188@gmail.com>
2012-11-20 00:22:08 +0800

    glSDR: added Waterfall class

---
+++ branches/android/aHPSDR/src/org/v1al/Waterfall.java
@@ -0,0 +1,30 @@
+package org.v1al;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+public class Waterfall extends GLSurfaceView implements OnTouchListener {
+
+	public Waterfall(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
