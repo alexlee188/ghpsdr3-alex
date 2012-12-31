e1817069d746dbef904449dd840db15fd05d37c6
Alex Lee <alexlee188@gmail.com>
2012-12-31 19:50:46 +0800

    dspserver: added Openssl callbacks for thread-safe operation

--- trunk/src/dspserver/client.c
+++ trunk/src/dspserver/client.c
@@ -852,6 +852,67 @@ SSL_CTX *evssl_init(void)
     return server_ctx;
 }
 
+static pthread_mutex_t *lock_cs;
+static long *lock_count;
+
+void pthreads_locking_callback(int mode, int type, char *file,
+	     int line);
+unsigned long pthreads_thread_id(void);
+
+void thread_setup(void)
+	{
+	int i;
+
+	lock_cs=OPENSSL_malloc(CRYPTO_num_locks() * sizeof(pthread_mutex_t));
+	lock_count=OPENSSL_malloc(CRYPTO_num_locks() * sizeof(long));
+	for (i=0; i<CRYPTO_num_locks(); i++)
+		{
+		lock_count[i]=0;
+		pthread_mutex_init(&(lock_cs[i]),NULL);
+		}
+
+	CRYPTO_set_id_callback((unsigned long (*)())pthreads_thread_id);
+	CRYPTO_set_locking_callback((void (*)())pthreads_locking_callback);
+	}
+
+void thread_cleanup(void)
+	{
+	int i;
+
+	CRYPTO_set_locking_callback(NULL);
+	fprintf(stderr,"cleanup\n");
+	for (i=0; i<CRYPTO_num_locks(); i++)
+		{
+		pthread_mutex_destroy(&(lock_cs[i]));
+		fprintf(stderr,"%8ld:%s\n",lock_count[i],
+			CRYPTO_get_lock_name(i));
+		}
+	OPENSSL_free(lock_cs);
+	OPENSSL_free(lock_count);
+
+	fprintf(stderr,"done cleanup\n");
+	}
+
+void pthreads_locking_callback(int mode, int type, char *file,
+	     int line){
+	if (mode & CRYPTO_LOCK)
+		{
+		pthread_mutex_lock(&(lock_cs[type]));
+		lock_count[type]++;
+		}
+	else
+		{
+		pthread_mutex_unlock(&(lock_cs[type]));
+		}
+}
+
+unsigned long pthreads_thread_id(void){
+	unsigned long ret;
+
+	ret=(unsigned long)pthread_self();
+	return(ret);
+}
+
 void* client_thread(void* arg) {
  
     int on=1;
@@ -909,6 +970,8 @@ void* client_thread(void* arg) {
         perror("client ctx init failed");
         exit(1);
     }
+    // setup openssl thread-safe callbacks
+    thread_setup();
 
     base = event_base_new();
 
@@ -926,6 +989,7 @@ void* client_thread(void* arg) {
     event_base_loop(base, 0);
 
     evconnlistener_free(listener);
+    thread_cleanup();
     SSL_CTX_free(ctx);
 
     return NULL;
