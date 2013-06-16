5e90aba4af3b07cdf8e3e9a878f3708c06d1a383
Rick Schnicker <rschnic@bigredbarn2.(none)>
2013-06-16 18:56:32 -0500

    Added and debugged functions in dspserver.

--- trunk/src/dspserver/client.c
+++ trunk/src/dspserver/client.c
@@ -1601,56 +1601,56 @@ void readcb(struct bufferevent *bev, void *ctx){
 
             SetANRvals(0,0,taps,delay,gain,leakage);
             SetANRvals(0,1,taps,delay,gain,leakage);
-        } else if(strncmp(cmd,"setrxagcattack",14)==0) {
+        } else if(strncmp(cmd,"setrxagcattack",14)==0) { // KD0OSS
             int value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atoi(tokens[0]);
             SetRXAGCAttack(0,0,value);
             SetRXAGCAttack(0,1,value);
-        } else if(strncmp(cmd,"setrxagcdecay",13)==0) {
+        } else if(strncmp(cmd,"setrxagcdecay",13)==0) { // KD0OSS
             int value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atoi(tokens[0]);
             SetRXAGCDecay(0,0,value);
             SetRXAGCDecay(0,1,value);
-        } else if(strncmp(cmd,"setrxagchang",12)==0) {
+        } else if(strncmp(cmd,"setrxagchang",12)==0) { // KD0OSS
             int value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atoi(tokens[0]);
             SetRXAGCHang(0,0,value);
             SetRXAGCHang(0,1,value);
-        } else if(strncmp(cmd,"setrxagchanglevel",17)==0) {
+        } else if(strncmp(cmd,"setrxagchanglevel",17)==0) { // KD0OSS
             double value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atof(tokens[0]);
             SetRXAGCHangLevel(0,0,value);
             SetRXAGCHangLevel(0,1,value);
-        } else if(strncmp(cmd,"setrxagchangthreshold",21)==0) {
+        } else if(strncmp(cmd,"setrxagchangthreshold",21)==0) { // KD0OSS
             int value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atoi(tokens[0]);
             SetRXAGCHangThreshold(0,0,value);
             SetRXAGCHangThreshold(0,1,value);
-        } else if(strncmp(cmd,"setrxagcthresh",14)==0) {
+        } else if(strncmp(cmd,"setrxagcthresh",14)==0) { // KD0OSS
             double value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atof(tokens[0]);
             SetRXAGCThresh(0,0,value);
             SetRXAGCThresh(0,1,value);
-        } else if(strncmp(cmd,"setrxagctop",11)==0) {
+        } else if(strncmp(cmd,"setrxagcmaxgain",15)==0) { // KD0OSS
             double value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
             value = atof(tokens[0]);
             SetRXAGCTop(0,0,value);
             SetRXAGCTop(0,1,value);
-        } else if(strncmp(cmd,"setrxagcslope",13)==0) {
+        } else if(strncmp(cmd,"setrxagcslope",13)==0) { // KD0OSS
             int value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
@@ -1731,7 +1731,7 @@ void readcb(struct bufferevent *bev, void *ctx){
             mode=atoi(tokens[0]);
             txMeterMode = mode;
             sdr_log(SDR_LOG_INFO,"SetTxMeterMode %d\n",mode);
-        } else if(strncmp(cmd,"setrxdcblockgain",14)==0) { // KD0OSS
+        } else if(strncmp(cmd,"setrxdcblockgain",16)==0) { // KD0OSS
             float gain;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
                 goto badcommand;
@@ -1833,6 +1833,99 @@ void readcb(struct bufferevent *bev, void *ctx){
             } else{
                 fprintf(stderr,"Invalid SetTXAMCarrierLevel command: '%s'\n",message);
             }
+        } else if(strncmp(cmd,"settxalcstate",13)==0) { // KD0OSS
+            int state;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            state=atoi(tokens[0]);
+            SetTXALCSt(1,state);
+        } else if(strncmp(cmd,"settxalcattack",14)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXALCAttack(1,value);
+        } else if(strncmp(cmd,"settxalcdecay",13)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXALCDecay(1,value);
+        } else if(strncmp(cmd,"settxalcbot",11)==0) { // KD0OSS
+            double value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atof(tokens[0]);
+            SetTXALCBot(1,value);
+        } else if(strncmp(cmd,"settxalchang",12)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXALCHang(1,value);
+        } else if(strncmp(cmd,"settxlevelerstate",17)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXLevelerSt(1,value);
+        } else if(strncmp(cmd,"settxlevelerattack",18)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXLevelerAttack(1,value);
+        } else if(strncmp(cmd,"settxlevelerdecay",17)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXLevelerDecay(1,value);
+        } else if(strncmp(cmd,"settxlevelerhang",16)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXLevelerHang(1,value);
+        } else if(strncmp(cmd,"settxlevelermaxgain",19)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetTXLevelerTop(1,value);
+        } else if(strncmp(cmd,"settxagcff",10)==0) { // KD0OSS
+            int state;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            state=atoi(tokens[0]);
+            SetTXAGCFF(1,state);
+        } else if(strncmp(cmd,"settxagcffcompression",21)==0) { // KD0OSS
+            double value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atof(tokens[0]);
+            SetTXAGCFFCompression(1,value);
+        } else if(strncmp(cmd,"setcorrecttxiqmu",16)==0) { // KD0OSS
+            double value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atof(tokens[0]);
+            SetCorrectTXIQMu(1,value);
+        } else if(strncmp(cmd,"setcorrecttxiqw",15)==0) { // KD0OSS
+            double value1;
+            double value2;
+            if (tokenize_cmd(&saveptr, tokens, 2) != 2)
+                goto badcommand;
+            value1=atof(tokens[0]);
+            value2=atof(tokens[0]);
+            SetCorrectTXIQW(1,value1,value2);
+        } else if(strncmp(cmd,"setfadelevel",12)==0) { // KD0OSS
+            int value;
+            if (tokenize_cmd(&saveptr, tokens, 1) != 1)
+                goto badcommand;
+            value=atoi(tokens[0]);
+            SetFadeLevel(0,0,value);
+            SetFadeLevel(0,1,value);
         } else if(strncmp(cmd,"setsquelchval",13)==0) {
             float value;
             if (tokenize_cmd(&saveptr, tokens, 1) != 1)
