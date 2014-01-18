a04ae5b2c7b6c7d8023cdb9a4c88d7a8123a8767
Alex Lee <alexlee188@gmail.com>
2014-01-18 16:23:17 +0800

    glSDR version 35

--- branches/android/aHPSDR/src/org/v1al/AHPSDRActivity.java
+++ branches/android/aHPSDR/src/org/v1al/AHPSDRActivity.java
@@ -1292,7 +1292,7 @@ public class AHPSDRActivity extends Activity implements SensorEventListener {
 		};
 		connection.start();
 		connection.sendCommand("q-master");
-	    connection.sendCommand("setClient glSDR(34)");
+	    connection.sendCommand("setClient glSDR(35)");
 		connection.setFrequency(frequency);
 		connection.setMode(mode);
 		connection.setBand(band);
