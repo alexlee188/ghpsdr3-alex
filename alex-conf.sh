4e4c64e8a39b7e2d78c436329a4dc27a435c4a41
Rob Frohne <rob.frohne@wallawalla.edu>
2014-05-06 10:35:02 -0700

    Added Sid's changes to build with the new codec2 changes.

--- alex-conf.sh
+++ alex-conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./configure CFLAGS='-fopenmp -flto -O3 -march=native' CXXFLAGS='-fopenmp -flto -O3 -march=native'
+./configure CFLAGS='-fopenmp -flto -O3 -march=native -I/usr/local/include/codec2' CXXFLAGS='-fopenmp -flto -O3 -march=native -I/usr/local/include/codec2'
 
