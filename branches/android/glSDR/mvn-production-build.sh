09932f302df1a45d807f0ea8c16ea31107bb13bf
Alex Lee <alexlee188@gmail.com>
2014-06-30 09:23:04 +0800

    glSDR: changed mvn-production-build.sh android adt path to android-studio.  Problem is zipsigner is not in android-studio (but in eclipse-adt)

--- branches/android/glSDR/mvn-production-build.sh
+++ branches/android/glSDR/mvn-production-build.sh
@@ -1,3 +1,3 @@
-mvn clean install -Dandroid.sdk.path="/home/alex/Desktop/adt-bundle-linux-x86_64-20140321/sdk"  -Dsign.keystore="/home/alex/Android_keystore" -Dsign.alias=android -Dsign.storepass=xxx  -Dsign.keypass=xxx
+mvn clean install -Dandroid.sdk.path="/home/alex/Desktop/android-studio/sdk"  -Dsign.keystore="/home/alex/Android_keystore" -Dsign.alias=android -Dsign.storepass=xxx  -Dsign.keypass=xxx
 
 
