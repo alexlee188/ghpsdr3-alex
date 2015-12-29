91b564d39b02d420b6767f07a82a31a5a7724ace
Philip Colllier <redcrown01@gmail.com>
2015-12-29 11:08:57 +0800

    Update LSBFilters.cpp
    Adjust passbands of LSB filters to cut more lows when narrow and pass more lows when wide.  This should be more suitable for ECSS broadcast reception, extended single sideband, and crisper narrow DX operations.

--- trunk/src/QtRadio/LSBFilters.cpp
+++ trunk/src/QtRadio/LSBFilters.cpp
@@ -30,17 +30,17 @@
 
 LSBFilters::LSBFilters() {
 
-    filters[0].init("5.0k",-5150,-150);
-    filters[1].init("4.4k",-4550,-150);
-    filters[2].init("3.8k",-3950,-150);
+    filters[0].init("5.0k",-5050,-50);
+    filters[1].init("4.4k",-4450,-50);
+    filters[2].init("3.8k",-3900,-100);
     filters[3].init("3.3k",-3450,-150);
     filters[4].init("2.9k",-3050,-150);
-    filters[5].init("2.7k",-2850,-150);
-    filters[6].init("2.4k",-2550,-150);
-    filters[7].init("2.1k",-2250,-150);
-    filters[8].init("1.8k",-1950,-150);
-    filters[9].init("1.0k",-1150,-150);
-    filters[10].init("Vari",-2550,-150);
+    filters[5].init("2.7k",-2900,-200);
+    filters[6].init("2.4k",-2600,-200);
+    filters[7].init("2.1k",-2400,-300);
+    filters[8].init("1.8k",-2150,-350);
+    filters[9].init("1.0k",-1400,-400);
+    filters[10].init("Vari",-5050,-50);
 
     selectFilter(3);
 }
