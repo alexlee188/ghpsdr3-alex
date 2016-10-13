6b175a7cab143677aaa6114397094c281ab9fb47
Alex Lee <alexlee188@gmail.com>
2016-10-13 22:42:06 +0800

    removed -flto compiler flag from alexconf.sh

--- alex-conf.sh
+++ alex-conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./configure CFLAGS='-fopenmp -flto -O3 -march=native -I/usr/local/include/codec2' CXXFLAGS='-fopenmp -flto -O3 -march=native -I/usr/local/include/codec2'
+./configure CFLAGS='-fopenmp -O3 -march=native -I/usr/local/include/codec2' CXXFLAGS='-fopenmp -O3 -march=native -I/usr/local/include/codec2'
 
